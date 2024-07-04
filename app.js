2d29165ebc705f684f618be4f32e9e522fb7bceb
RusithTharindu <rusiththarindu2@gmail.com>
2024-07-04 19:07:09 +0530

    feat: simple middleware

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const app = express();
 const user = require('./routers/user')
 const student = require('./routers/student')
 
+app.use(log); //using the middle ware
 app.use('/api/users', user);
-app.use('/api/students',student)
+app.use('/api/students',student);
+
+//middleware
+function log(req, res, next) {
+    console.log('This is a middleware');
+    next();
+}
 
 console.log(process.env.PORT);
 
