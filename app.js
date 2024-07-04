4ec3f79cf6f7845a59a47f95c18f448f9593b4ad
RusithTharindu <rusiththarindu2@gmail.com>
2024-07-04 22:21:28 +0530

    In built middleware

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 const  express = require('express');
 const app = express();
 const user = require('./routers/user')
-const student = require('./routers/student')
+const student = require('./routers/student');
+const morgan = require('morgan');
 
+app.use(express.json()); //using a built-in middleware
+app.use(morgan('tiny')); //tiny will show the time for the response
 app.use(log); //using the middle ware
-app.use('/api/users', user);
-app.use('/api/students',student);
+app.use('/api/users',userMid, user);
+app.use('/api/students',studentMid ,student);
+
+if (app.get('env') === "development") {
+    app.use(morgan('dev'));
+}
+
 
 //middleware
 function log(req, res, next) {
     console.log('This is a middleware');
+    req.id = "10";
     next();
 }
 
-console.log(process.env.PORT);
+function userMid(req, res, next) {
+    console.log('This is a middleware for USER');
+    next();
+}
+
+function studentMid(req, res, next) {
+    console.log('This is a middleware for Student');
+    next();
+}
+
+// console.log(process.env.PORT);
+morgan('tiny');
 
 const port = process.env.PORT || 3000;
 
--- routers/user.js
+++ routers/user.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const morgan = require('morgan')
 
-router.get('/', (req, res) => {
+//for the api with same route
+router.route('/')
+.get((req, res) => {
     res.send({"msg": "Hello GET user"});
-});
+})
 
-router.get('/add', (req, res) => {
-    res.send("Get request user from api/users/add");
-});
+.post((req, res) => {
+    // res.send({"msg": "Hello POST user"});
+
+    //using express.json middleware
+    // res.send(req.body); 
+    //using the built-in middleware (here, without the express.json() middleware, req.body will not give any response)
+
+    res.send(req.body.data) 
+    //data is the type of middleware ex: { "data" : "abc" }
+    //above response will result, abc
+
+    //using morgon middleware
+
+})
+
+// router.get('/', (req, res) => {
+//     res.send({"msg": "Hello GET user"});
+//     res.send(req.id);
+// });
+
+// router.get('/add', (req, res) => {
+//     res.send("Get request user from api/users/add");
+// });
 
 router.post('/', (req, res) => {
     res.send({"msg": "Hello POST user"});
